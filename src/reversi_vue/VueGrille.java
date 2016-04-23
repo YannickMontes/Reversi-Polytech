c65ea4e8d378130722dd72e3dca5570cb35fc433
MONTES Yannick <montes.yannick@gmail.com>
2016-04-23 12:40:41 +0200

    Ajout du focus lors du passage sur une case, et ajout du jeu possible.

--- src/reversi_vue/VueGrille.java
+++ src/reversi_vue/VueGrille.java
@@ -10,7 +10,11 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import javax.swing.JComponent;
+import reversi_modele.Case;
 import reversi_modele.CaseContent;
 import reversi_modele.Grille;
 
@@ -18,13 +22,19 @@ import reversi_modele.Grille;
  *
  * @author yannick
  */
-public class VueGrille extends JComponent
+public class VueGrille extends JComponent implements MouseMotionListener, MouseListener
 {
     private Grille grille;
+    private CaseContent nextTurn;
+    private Case focused;
 
     public VueGrille(Grille g)
     {
         super();
+        focused = null;
+        this.nextTurn = CaseContent.NOIR;
+        this.addMouseMotionListener(this);
+        this.addMouseListener(this);
         this.grille = g;
     }                   
 
@@ -49,9 +59,20 @@ public class VueGrille extends JComponent
         {
             for(int j=0; j<Grille.WIDTH_GRID; j++)
             {
-                if(this.grille.getCase(i, j).getVal()!=CaseContent.VIDE)
+                if(this.grille.getCase(i, j).getVal()!=CaseContent.VIDE && this.grille.getCase(i,j)!=focused)
                 {
                     g2.drawImage(this.grille.getCase(i, j).getImage(), j*100, i*100, this);
+                }
+                else if(this.grille.getCase(i, j) == focused)
+                {
+                    if(nextTurn == CaseContent.NOIR)
+                    {
+                        g2.drawImage(Case.pionNoirFocus, j*100, i*100, this);
+                    }
+                    else
+                    {
+                        g2.drawImage(Case.pionBlancFocus, j*100, i*100, this);
+                    }
                 }
             }
         }
@@ -62,4 +83,64 @@ public class VueGrille extends JComponent
     {
         return new Dimension(801,801);
     }                 
+
+    @Override
+    public void mouseDragged(MouseEvent e)
+    {
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e)
+    {
+        int i = e.getY()/100;
+        int j = e.getX()/100;
+        if(this.grille.isPlayable(e.getY()/100, e.getX()/100, nextTurn))
+        {
+            focused = this.grille.getCase(i, j);
+        }
+        else
+        {
+            focused = null;
+        }
+        this.repaint();
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e)
+    {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e)
+    {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e)
+    {
+        if(this.grille.isPlayable(e.getY()/100, e.getX()/100, nextTurn))
+        {
+            this.grille.executeTurn(nextTurn,e.getY()/100, e.getX()/100);
+            if(nextTurn == CaseContent.BLANC)
+            {
+                nextTurn = CaseContent.NOIR;
+            }
+            else
+            {
+                nextTurn = CaseContent.BLANC;
+            }
+            System.out.println(this.grille);
+            this.repaint();
+        }
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e)
+    {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e)
+    {
+    }
 }
